e5cc1014a108bf6d04f27d815b3cd380a7edbacc
Bruemor <arymer1@gmail.com>
2018-05-08 18:42:16 -0400

    Initial project set up push

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+
+console.log("Hello World!");
+
+var test = new Vue ({
+    el: '#vue',
+    data: {
+        message: 'This is working?'
+    }
+})
+
+import '../src/index.scss';
+
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const HtmlWebpackPlugin = require("vue-html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: '/node_modules/',
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function() {
+                                return [
+                                    require('autoprefixer')
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            vue: true,
+            title: 'My Custom Template',
+            template: './src/index.html'
+        })
+    ]
+}
